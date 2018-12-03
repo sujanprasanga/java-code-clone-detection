e80d21740f9443ed9014c11173f6b776578b92f6
Sujan Kotinkaduwa <sujan.kotinkaduwa@cambio.se>
2018-12-03 06:51:42 +0530

    commit before adding line numbers

--- src/main/java/lk/ac/mrt/cse/mscresearch/codeclones/bytecode/instructions/PrimitiveOp.java
+++ src/main/java/lk/ac/mrt/cse/mscresearch/codeclones/bytecode/instructions/PrimitiveOp.java
@@ -4,14 +4,18 @@ import java.util.regex.Matcher;
 
 public class PrimitiveOp extends Instruction{
 
-	private final static ImmutableUtil<PrimitiveOp> immutableUtil = new ImmutableUtil<>((String arg, Matcher matcher, String tos)-> new PrimitiveOp(arg));
+//	private final static ImmutableUtil<PrimitiveOp> immutableUtil = new ImmutableUtil<>(arg-> new PrimitiveOp(arg));
+	
+	public static Instruction from(InstructionCreateParam p) {
+		return get(p.arg, p.matcher);
+	}
 	
 	private PrimitiveOp(String ops) {
 		super(0, TYPE.PRIMITIVE_OP);
 	}
 	
 	public static PrimitiveOp get(String ops, Matcher matcher){
-		return immutableUtil.get("");
+		return new PrimitiveOp("");
 	}
 	
 	public void resolvePreConditions(String[] body, int index){}
